d9af5a1e0f67ad97050d99913a42b444108856ae
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-16 10:23:36 +0100

    Ajout d'un commentaire en début d escript expliquant le service worker

---
+++ sw.js
@@ -0,0 +1,6 @@
+/**
+ * Script exécuté en tâche de fond par le navigateur.
+ * Joue le rôle d'intermédiaire entre le client et le serveur.
+ * Permet de mettre en cache des données utilisable
+ * lorsqu'il n'y aura plus de réseaux
+ */
