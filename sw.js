b0a431e4aa3796c3796f8fb7f401aefac5df4e04
Rabie Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-17 09:42:04 +0100

    rectification

--- sw.js
+++ sw.js
@@ -16,6 +16,7 @@ this.addEventListener("install", function (event) {
         "/",
         "/index.html",
         "/css/style.css",
+        "/css/background.css",
         "/js/apiRequest.js",
         "/js/controller.js",
         "/js/ihm.js",
@@ -38,4 +39,21 @@ this.addEventListener("install", function (event) {
   );
 });
 
-self.addEventListener("fetch", () => {});
+
+self.addEventListener("fetch", (event) => {
+  // Nous voulons seulement répondre aux requêtes concernant notre application en testant l'URL de la requête
+  if (event.request.url.startsWith("http://localhost:3000/")) {
+    // Tente de produire une réponse à la requête fetch interceptée
+    event.respondWith(
+        caches.open(STATIC_CACHE_NAME)
+            // En allant chercher la réponse dans le cache en premier
+            .then(cache => cache.match(event.request))
+            // Puis sur le réseau si elle n'existe pas dans le cache (cacheRequest === undefined)
+            .then(cacheRequest => cacheRequest || fetch(event.request))
+    );
+  }
+  // Si votre condition if() est fausse, alors cet écouteur d'événement ne proposera pas de réponse à la requête.
+  // Cela laisse l'opportunité à d'autres écouteurs d'événements d'intercepter cette requête pour produire une réponse adaptée.
+  // Si aucun écouteur d'événement ne propose de réponse à cette requête, alors la requête sera gérée par votre navigateur
+  // comme si aucun "Service Worker" n'était enregistré
+});
