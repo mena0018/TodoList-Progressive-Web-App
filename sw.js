6d2a57c84c46ff4148723bfefc0588a4a65ba8a4
Menad <rabie.menad@etudiant.univ-reims.fr>
2022-01-23 15:52:20 +0100

    Modification de la stratégie de gestion du cache - ajout de la stratégie stale-while-revalidate

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * lorsqu'il n'y aura plus de réseaux
  */
 
-const STATIC_CACHE_NAME = "todosApp.v0";
+const STATIC_CACHE_NAME = "todosApp.v2";
 
 this.addEventListener("install", function (event) {
   // Garantit que le Service Worker ne s'installera pas tant que le code à l'intérieur de
@@ -40,22 +40,20 @@ this.addEventListener("install", function (event) {
 });
 
 
-self.addEventListener("fetch", (event) => {
-  // Nous voulons seulement répondre aux requêtes concernant notre application en testant l'URL de la requête
+self.addEventListener('fetch', function (event) {
   if (event.request.url.startsWith("http://localhost:3000/")) {
-    // Tente de produire une réponse à la requête fetch interceptée
     event.respondWith(
-        caches.open(STATIC_CACHE_NAME)
-            // En allant chercher la réponse dans le cache en premier
-            .then(cache => cache.match(event.request))
-            // Puis sur le réseau si elle n'existe pas dans le cache (cacheRequest === undefined)
-            .then(cacheRequest => cacheRequest || fetch(event.request))
+      caches.open(STATIC_CACHE_NAME).then(function (cache) {
+        return cache.match(event.request).then(function (response) {
+          var fetchPromise = fetch(event.request).then(function (networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+          return response || fetchPromise;
+        });
+      }),
     );
   }
-  // Si votre condition if() est fausse, alors cet écouteur d'événement ne proposera pas de réponse à la requête.
-  // Cela laisse l'opportunité à d'autres écouteurs d'événements d'intercepter cette requête pour produire une réponse adaptée.
-  // Si aucun écouteur d'événement ne propose de réponse à cette requête, alors la requête sera gérée par votre navigateur
-  // comme si aucun "Service Worker" n'était enregistré
 });
 
 
